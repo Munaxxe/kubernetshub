0263dd8c97e563c7fabaf64bc39aa3b21f1de7e1
i_am_mUNAxxe <85413674+Munaxxe@users.noreply.github.com>
2024-02-27 09:15:12 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 	//Hide Loading Box (Preloader)
 	function handlePreloader() {
 		if($('.preloader').length){
-			$('.preloader').delay(500).fadeOut(2000);
+			$('.preloader').delay(50).fadeOut(2000);
 		}
 	}
 	
